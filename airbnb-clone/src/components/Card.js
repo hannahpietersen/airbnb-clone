c221dff607781e7273c62bad73c54bebfa613dd9
someid <hannahpietersen@gmail.com>
2023-02-24 17:13:55 +0530

    added code to respective files

---
+++ airbnb-clone/src/components/Card.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Card(props){
+    return (
+        <div className="card">
+           <img src="../images/katie-zaferes.png" alt="katie-zaferes" className="card--image"/>
+           <div className="card--stats">
+              <img src="..//images/star.png" alt="star-pic" className="card--star"/>
+              <span>5.0</span>
+              <span className="gray">(6) •</span>
+              <span className="gray">USA</span>
+              <p>Life Lessons with Katie Zaferes</p>
+              <p><span className="bold">From $136</span> / person</p>
+           </div>
+        </div>
+    )
+}

903a0c5e326b609b19276912e92f6cce2668fc16
someid <hannahpietersen@gmail.com>
2023-02-25 14:07:28 +0530

    added code to respective files

--- airbnb-clone/src/components/Card.js
+++ airbnb-clone/src/components/Card.js
@@ -1,16 +1,25 @@
 import React from "react"
 
 export default function Card(props){
+
+    let badgeText;
+    if(props.item.openSpots === 0){
+        badgeText = "SOLD OUT"
+    } else if(props.item.location === "Online"){
+        badgeText = "ONLINE"
+    }
+    
     return (
         <div className="card">
-           <img src="../images/katie-zaferes.png" alt="katie-zaferes" className="card--image"/>
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+           <img src={props.item.coverImg} alt="katie-zaferes" className="card--image"/>
            <div className="card--stats">
               <img src="..//images/star.png" alt="star-pic" className="card--star"/>
-              <span>5.0</span>
-              <span className="gray">(6) •</span>
-              <span className="gray">USA</span>
-              <p>Life Lessons with Katie Zaferes</p>
-              <p><span className="bold">From $136</span> / person</p>
+              <span>{props.item.stats.rating}</span>
+              <span className="gray">({props.item.stats.reviewCount}) • </span>
+              <span className="gray">{props.item.location}</span>
+              <p className="card--title">{props.item.title}</p>
+              <p className="card--price"><span className="bold">From ${props.item.price}</span> / person</p>
            </div>
         </div>
     )
